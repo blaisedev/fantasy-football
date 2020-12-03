7c5a38dc624d28e5f62a75da65a8aad7ad50c0b9
blaisedev <blaisedevaney@gmail.com>
2020-12-03 13:28:29 +0000

    project

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {Platform, Text} from 'react-native';
+import {WebView} from "react-native-webview";
+import WebText from "react-native-web/dist/exports/Text";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (Platform.OS === 'web' ? <WebText>Not supported in web</WebText> :
+            <WebView source={{uri: 'https://fantasy.premierleague.com'}}
+                     style={{flex: 1, height: 2, marginTop: 20}}
+            />
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
