ee309a964e2f50a616a4f69c0ca68df0f2415f63
blaisedev <blaisedevaney@gmail.com>
2020-12-03 15:50:26 +0000

    cleanupo

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import * as React from "react";
-import {Platform, Text} from 'react-native';
 import {WebView} from "react-native-webview";
-import WebText from "react-native-web/dist/exports/Text";
 
 export default function App() {
-    return (Platform.OS === 'web' ? <WebText>Not supported in web</WebText> :
-            <WebView source={{uri: 'https://fantasy.premierleague.com'}}
-                     style={{flex: 1, height: 2, marginTop: 20}}
-            />
-    )
+    return <WebView source={{uri: 'https://fantasy.premierleague.com'}}
+                    style={{flex: 1, height: 2, marginTop: 20}}
+    />
+
 }
